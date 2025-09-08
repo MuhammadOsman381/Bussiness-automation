08ccb1d4b50449170511c23ca9a77ba872673e7d
MuhammadOsman381 <mosman257@gmail.com>
2025-09-08 18:47:13 +0500

    api url changed

--- client/src/helpers/Helpers.ts
+++ client/src/helpers/Helpers.ts
@@ -1,7 +1,7 @@
 class Helpers {
     static localhost: string = 'localhost:8000';
     static server: string = '3.91.226.230/backend';
-    static basePath: string = `http://${this.localhost}`;
+    static basePath: string = `http://${this.server}`;
     static apiUrl: string = `${this.basePath}/api/`;
     static secretKey: string = '3434774438';
     static imageUrl: string = `${this.basePath}/files/`;
